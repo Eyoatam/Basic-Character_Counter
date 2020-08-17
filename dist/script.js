9b9808596c74f13fffa029d523dc769dff053366
Tame <tame@Eyoatams-MacBook-Pro.local>
2020-08-17 07:38:13 +0300

    add the source files

---
+++ dist/script.js
@@ -0,0 +1,11 @@
+const TextArea = document.getElementById("message");
+const RemainingCharacters = document.getElementById("remaining-characters");
+const MaxChars = 500;
+
+TextArea.addEventListener("input", () => {
+  const Remaining = MaxChars - TextArea.value.length;
+  const Danger = Remaining < MaxChars * 0.1 ? "red" : null;
+
+  RemainingCharacters.textContent = `${Remaining} Characters Remaining`;
+  RemainingCharacters.style.color = Danger;
+});
