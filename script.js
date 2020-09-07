f9d3186715ba18f56207f2035a68446374265115
Eyoatam Tamirat <eyoatamtamirat7@gmail.com>
2020-09-07 17:15:25 +0300

    Make the textarea readonly when it reaches 0

--- script.js
+++ script.js
@@ -5,11 +5,8 @@ const MaxChars = 500;
 TextArea.addEventListener("input", () => {
   const Remaining = MaxChars - TextArea.value.length;
   const Danger = Remaining < MaxChars * 0.1 ? "red" : null;
+  const Stop = Remaining < 0 ? (TextArea.readOnly = true) : null;
 
   RemainingCharacters.textContent = `${Remaining} Characters Remaining`;
   RemainingCharacters.style.color = Danger;
-  
-  if(TextArea.input.value.length === 500) {
-    TextArea.readonly = true;
-  }
 });
