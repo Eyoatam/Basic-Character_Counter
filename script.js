f8ee337f313fd759e1f560bd326dd0b018023e0a
Eyoatam Tamirat <eyoatamtamirat7@gmail.com>
2020-09-07 17:15:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ const MaxChars = 500;
 TextArea.addEventListener("input", () => {
   const Remaining = MaxChars - TextArea.value.length;
   const Danger = Remaining < MaxChars * 0.1 ? "red" : null;
-  const Stop = Remaining < 0 ? (TextArea.readOnly = true) : null;
-
   RemainingCharacters.textContent = `${Remaining} Characters Remaining`;
   RemainingCharacters.style.color = Danger;
 });
